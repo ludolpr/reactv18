e5125f5e2bf185a4cd3a62bbb937d3207f006a56
ludolpr <ludolpr@gmail.com>
2022-10-12 16:36:58 +0200

    fix erreur cours ok

---
+++ src/pages/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi, id
+        qui veritatis cum, omnis optio enim consectetur, facere a deleniti non.
+        Ipsa, quod quibusdam corporis repellendus iure iusto quia ratione
+        tenetur! Quia sapiente eum velit necessitatibus ea voluptas sint qui
+        autem eos. Recusandae esse fugiat architecto dolores maxime. Error
+        suscipit nam repellat magni molestiae enim? Laudantium voluptate
+        pariatur accusantium soluta consectetur? Vero eos facere officia quas
+        facilis magni dolor voluptas culpa suscipit, mollitia iure dolore
+        reprehenderit. Aspernatur nemo eos, necessitatibus, praesentium
+        voluptate sequi saepe aliquam dignissimos voluptas nostrum quis illo
+        deserunt laborum tempore accusamus. Perferendis voluptates itaque
+        debitis eaque corrupti.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        praesentium expedita repellendus ducimus quis tempora distinctio
+        cupiditate repellat alias quaerat vero aspernatur voluptate provident
+        fugiat atque iusto, hic numquam autem quia ea! Veniam, sed magni autem
+        earum asperiores doloribus architecto provident aut dolorum laboriosam
+        saepe ab unde dolor temporibus nesciunt voluptate eius vitae rerum
+        repudiandae cum officiis libero voluptates! Culpa quo reprehenderit a
+        voluptatum enim sequi facere officia expedita quibusdam exercitationem
+        placeat, totam unde facilis rerum sunt dignissimos non debitis tempore,
+        similique, consequuntur perspiciatis nesciunt? Ipsam explicabo, alias
+        repudiandae porro architecto inventore asperiores deserunt sapiente
+        facere. Atque, a modi provident accusamus id asperiores quod consectetur
+        dignissimos dolores impedit, dolor dolore, iste consequuntur earum
+        eveniet velit reprehenderit ipsum nulla labore quibusdam numquam
+        mollitia! Quisquam culpa voluptatibus iure asperiores nam illum itaque
+        rem repellendus mollitia in explicabo quasi delectus officiis
+        praesentium sapiente nobis non, magni, earum soluta quo. Suscipit
+        praesentium facere cupiditate mollitia, modi recusandae debitis magnam
+        quas natus cum ea porro. Commodi omnis, culpa quod illo quaerat, unde
+        quis ad accusamus quidem vel ex accusantium id error saepe, obcaecati
+        similique consequuntur minima deserunt repudiandae quisquam? Saepe
+        magnam vitae unde consectetur soluta, molestiae inventore! Deleniti,
+        quam nam nulla ad adipisci enim esse?
+      </p>
+    </div>
+  );
+};
+
+export default About;
